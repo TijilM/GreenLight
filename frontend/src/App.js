3a0a904169f5a60176ce7e5bf9f5796dec4cd42a
tijil <tijilmalhotra@gmail.com>
2023-01-02 13:08:20 +0530

    working on login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import Landing  from "./pages/landing";
-
-
+import Login from "./pages/login";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 function App() {
   return (
     <div className="App">
-      <Landing/>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Landing />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        {/* <Route path="/signup" element={<Signup />}></Route> */}
+        </Routes>
+    </Router>
+  </div>
   );
 }
 
