93de6fc43dd5173ffccbb0ae6ab61b83af9bce96
tijil <tijilmalhotra@gmail.com>
2023-01-14 22:37:09 +0530

    working on classrooms

--- frontend/src/pages/home/Components/classroom.js
+++ frontend/src/pages/home/Components/classroom.js
@@ -1,43 +1,47 @@
-import React, { useEffect ,useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from "../Style/classroom.module.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCamera } from '@fortawesome/free-solid-svg-icons'
+import camera from "../Assets/camera-solid.svg"
 export default function classroom(props) {
-  return (
-    
-    <>
-    {props.classroom.isLightOn ?
-  
-    <div className={styles.classRed}>
-        <div className={styles.classData}>
-            <p>{props.classroom.name}  </p>
-        </div>
-        <div className={styles.classData}>
-            <p>  Status : On</p>
-        </div>
-        <div className={styles.classData}>
-            <i class="fa-solid fa-camera"></i>
-        </div>
-        <div className={styles.classData}>
-            
-        </div>
+    return (
 
-    </div>
-    :
-    <div className={styles.classBlue}>
-    <div className={styles.classData}>
-        <p>{props.classroom.name}  </p>
-    </div>
-    <div className={styles.classData}>
-        <p  >Status : Off</p>
-    </div>
-    <div className={styles.classData}>
-        <i class="fa-solid fa-camera"></i>
-    </div>
-    <div className={styles.classData}>
-        
-    </div>
+        <>
+            {props.classroom.isLightOn ?
 
-</div>
-  }
-  </>
-  )
+                <div className={styles.classRed}>
+                    <div className={styles.classData}>
+                        <p>{props.classroom.name}  </p>
+                    </div>
+                    <div className={styles.classData}>
+                        <p>  Status : On</p>
+                    </div>
+                    <div className={styles.classData}>
+                        <div className={styles.iconContainer}>
+                            <button>
+                                <img src={camera} style={{ height: "1rem", width: "30px" }} />
+                            </button>
+                        </div>
+                    </div>
+
+                </div>
+                :
+                <div className={styles.classBlue}>
+                    <div className={styles.classData}>
+                        <p>{props.classroom.name}  </p>
+                    </div>
+                    <div className={styles.classData}>
+                        <p  >Status : Off</p>
+                    </div>
+                    <div className={styles.classData}>
+                        <button>
+                            <img src={camera} style={{ height: "1rem", width: "30px", }} />
+                        </button>
+                    </div>
+
+
+                </div>
+            }
+        </>
+    )
 }
--- frontend/src/pages/home/Components/classrooms.js
+++ frontend/src/pages/home/Components/classrooms.js
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 
 function Classrooms() {
     class myclassRooms {
-        constructor(name, cam_url,status) {
+        constructor(name, cam_url, status) {
             this.name = name;
             this.cam_url = cam_url;
             this.isLightOn = status
@@ -17,74 +17,79 @@ function Classrooms() {
     const [classRooms, setclassRooms] = useState([]);
     const effectRan = useRef(false);
     useEffect(() => {
-        const interval = setInterval(()=>{
-        if (effectRan.current === false) {
-            const fetchData = () => {
-                return fetch("https://run.mocky.io/v3/f8285463-7b6c-48d2-9ca3-56201bd19f8e", { method: "GET" }
-                )
-                    .then(res => {
-                        return res.json();
-                    })
-                    .then((data) => {
-                        var m = data.length;
-                        setclassRooms([]);
-                        for (var i = 0; i < m; i++) {
-                            let iclass = new myclassRooms(data[i].name, data[i].cam_url, data[i].status);
-                            setclassRooms(classRooms => [...classRooms, iclass]);
-                        }
-                    })
+        const interval = setInterval(() => {
+            if (effectRan.current === false) {
+                const fetchData = () => {
+                    return fetch("https://run.mocky.io/v3/f8285463-7b6c-48d2-9ca3-56201bd19f8e", { method: "GET" }
+                    )
+                        .then(res => {
+                            return res.json();
+                        })
+                        .then((data) => {
+                            var m = data.length;
+                            setclassRooms([]);
+                            for (var i = 0; i < m; i++) {
+                                let iclass = new myclassRooms(data[i].name, data[i].cam_url, data[i].status);
+                                setclassRooms(classRooms => [...classRooms, iclass]);
+                            }
+                        })
 
-            };
-            fetchData();
-        }
-        return  () =>{
-            effectRan.current=true;
-        }},5000);
-        
+                };
+                fetchData();
+            }
+            return () => {
+                effectRan.current = true;
+            }
+        }, 3000);
+
     }, []);
     const authCtx = useContext(AuthContext)
     return (
         <>
-        <div className={styles.logout}>
-            <i class="fa fa-sign-out" aria-hidden="true"></i>
-            <a className={styles.logoutLink} >
-            <Link  onClick={authCtx.logout} to="/"  >LOGOUT</Link> 
-            </a>
-        </div>
-        <div className={styles.container}>
-            <div className={styles.monitorHead} >
-                <center><h1 className={styles.monitorHeading}>GreenLight Monitor System</h1>
-                    <hr
-                        style={{
-                            background: 'rgb(93, 226, 93)',
-                            color: 'rgb(93, 226, 93)',
-                            borderColor: 'rgb(93, 226, 93)',
-                            height: '0.1px',
-                            width: '55%',
-                        }}
-                    />
-                    <p className={styles.monitorPara}>Here is a list of all the classrooms. Their color depicts the status of the lights in the respective classroom:</p>
 
-                </center>
+            <div className={styles.container}>
+                <div className={styles.monitorHead} >
+                    <center><h1 className={styles.monitorHeading}>GreenLight Monitor System</h1>
+                        <hr
+                            style={{
+                                background: 'rgb(93, 226, 93)',
+                                color: 'rgb(93, 226, 93)',
+                                borderColor: 'rgb(93, 226, 93)',
+                                height: '0.1px',
+                                width: '55%',
+                            }}
+                        />
+                        <p className={styles.monitorPara}>Here is a list of all the classrooms. Their color depicts the status of the lights in the respective classroom:</p>
 
-            </div>
-            <div className={styles.classes}>
-                <div className={styles.classrooms}>
-                    {classRooms.length === 0 ? "No Todos Left" :
-                        classRooms.map((classroom) => (
-                            <div id={classroom.name}>
-                                <Classroom classroom={classroom} />
-                            </div>
-                        ))
-                    }
+                    </center>
+
                 </div>
+                <div className={styles.classes}>
+                    <div className={styles.classrooms}>
+                        {classRooms.length === 0 ? "Data is Loading" :
+                            classRooms.map((classroom) => (
+                                <div id={classroom.name}>
+                                    <Classroom classroom={classroom} />
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
+                <center >
+                    <div className={styles.btnContainer}>
+                        
+                    <div className={styles.addRoom}>
+                        <a className={styles.addRoomLink} href="#">Add Room +</a>
+                    </div>
+                    <div className={styles.logout}>
+                        <i class="fa fa-sign-out" aria-hidden="true"></i>
+                        <a className={styles.logoutLink} >
+                            <Link onClick={authCtx.logout} to="/" style={{color:'grey'}} >LOGOUT</Link>
+                        </a>
+                    </div>
+                    </div>
+                </center>
             </div>
-            <center>
-            <div className={styles.addRoom}>
-                <a className={styles.addRoomLink} href="#">Add Room +</a>
-            </div>
-            </center>
-        </div>
         </>
     )
 }
